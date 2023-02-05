6d9789b64c434b2b45d051b26bac844c302cf1b3
jaaaaum <jauzim@gmail.com>
2023-02-05 19:36:18 -0300

    component

---
+++ new_ng/src/app/component-first/component-first.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-first',
+  templateUrl: './component-first.component.html',
+  styleUrls: ['./component-first.component.scss']
+})
+export class ComponentFirstComponent {
+
+}
